4b9af6919e610cb0fd48f43aecdc218d004e9b83
shivkant15 <114304302+shivkant15@users.noreply.github.com>
2022-11-15 12:26:16 +0530

    Day 01 BAsic First Program , Addition in Java

---
+++ Day 01/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void main(String args[]){
+
+        int x=18;
+        int y=26;
+        int sum;
+        sum=x+y;
+        System.out.println(sum);
+    }
+}
---
+++ Day 01/Test4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.math.*
+
+public class Test4 {
+
+    int a,b,c,ans1,ans;
+
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+
+        int ans1=Maths  max(a,b);
+        int ans=Maths max(b,c);
+
+        System.out.println(ans);
+
+    }
+
+}
